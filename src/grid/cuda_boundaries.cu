0a0ed8f9ace12243dfd7e8b66d7affb1c1e4ffa4
hjl28 <hjl28@pitt.edu>
2026-04-07 05:37:28 -0400

    ICs

--- src/grid/cuda_boundaries.cu
+++ src/grid/cuda_boundaries.cu
@@ -311,7 +311,7 @@ __global__ void Wind_Boundary_kernel(Real *c_device, int nx, int ny, int nz, int
 
   Real density, velocity_x, velocity_y, velocity_z, pressure, number_density;
 
-  number_density   = 1e-2;  // same value as n_bg in cloud initial condition function (cm^-3)
+  number_density   = 1e-3;  // same value as n_bg in cloud initial condition function (cm^-3)
   Real temperature = 1e6;   // same value as T_bg in cloud initial condition function (K)
 
   // same values as rho_bg and p_bg in cloud initial condition function
