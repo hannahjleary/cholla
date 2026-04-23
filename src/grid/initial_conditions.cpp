df6f86f4f9b8f7bdd724ae80416a3b8a5d3015f9
hjl28 <hjl28@pitt.edu>
2026-04-23 01:59:43 -0400

    ICs

--- src/grid/initial_conditions.cpp
+++ src/grid/initial_conditions.cpp
@@ -1324,7 +1324,7 @@ void Grid3D::Clouds(ParameterMap &pmap)
   Real p_bg, p_cl;       // background and cloud pressure
   Real mu   = 0.6;       // mean atomic weight
   int N_cl  = 1;         // number of clouds
-  Real R_cl = 0.02;     // cloud radius in code units (kpc)
+  Real R_cl = 0.05;     // cloud radius in code units (kpc)
   Real cl_pos[N_cl][3];  // array of cloud positions
   Real r;
 
@@ -1339,14 +1339,14 @@ void Grid3D::Clouds(ParameterMap &pmap)
 
   // single centered cloud setup
   for (int nn = 0; nn < N_cl; nn++) {
-    cl_pos[nn][0] = 0.1 * H.xdglobal;
+    cl_pos[nn][0] = 0.5 * H.xdglobal;
     cl_pos[nn][1] = 0.5 * H.ydglobal;
     cl_pos[nn][2] = 0.5 * H.zdglobal;
     printf("Cloud positions: %f %f %f\n", cl_pos[nn][0], cl_pos[nn][1], cl_pos[nn][2]);
   }
 
-  n_bg   = 1.0e-3;
-  n_cl   = 0.1;
+  n_bg   = 5.0e-3;
+  n_cl   = 5.0e-1;
   rho_bg = n_bg * mu * MP / DENSITY_UNIT;
   rho_cl = n_cl * mu * MP / DENSITY_UNIT;
   printf("Cloud initial density: %e\n", rho_cl);
@@ -1357,7 +1357,7 @@ void Grid3D::Clouds(ParameterMap &pmap)
   printf("Cloud initial density: %e\n", rho_cl);
   printf("Wind initial density: %e\n", rho_bg);
 #endif
-  vx_bg = 100 * TIME_UNIT / KPC;
+  vx_bg = 1000 * TIME_UNIT / KPC;
   // vx_c  = -200*TIME_UNIT/KPC; // convert from km/s to kpc/kyr
   vx_cl = 0 * TIME_UNIT / KPC;
   vy_bg = vy_cl = 0.0;
